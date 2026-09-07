25b970049abd5fdde4f9637e237759b46d397bfe
Trần Thị Phương Hướng <huongpq02@gmail.com>
2026-09-08 00:00:40 +0700

    feat(auth): validate email availability during registration

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,10 +1,29 @@
-class AuthService {
-  async register(data: any) {
-    return {
-      message: "Register service called",
-      user: data,
-    };
-  }
+import pool from "../config/database.config";
+interface RegisterData {
+  full_name: string;
+  email: string;
+  password: string;
+  phone?: string;
+  address?: string;
 }
+class AuthService {
+  async register(data: RegisterData) {
+    const existingUser = await pool.query(
+    `
+    SELECT user_id
+    FROM users
+    WHERE email = $1
+  `,
+  [data.email]
+);
+        if (existingUser.rows.length > 0) {
+  throw new Error("Email already exists");
+}
+return {
+  message: "Email is available",
+  user: data,
 
+};
+}
+}
 export default new AuthService();
